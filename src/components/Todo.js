9c201a47081c9afecd3385058e75e3802478993c
nardosteve <jackcale50@gmail.com>
2022-08-04 18:26:58 +0300

    Fixed adding to list issue

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,30 @@
 import React, {useState} from 'react'
-import TodoForm from "./TodoForm"
+//Import icons
+import {RiCloseCircleLine} from 'react-icons/ri'
+import {TiEdit} from 'react-icons/ti'
 
-function Todo() {
+
+function Todo({todos, completeTodo}) {
     const [edit, setEdit] = useState({
-        id: null.,
+        id: null,
         value: ''
-    })
+    });
 
-  return (
-    <div className={Todo.isComplete ? 'todo-row complete' : 'todo-row'}
+  return todos.map((todo, index) => (
+    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
     key={index}>
 
-        <div key={Todo.id} onClick={() => completeTodo(todo.id)}>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
         </div>
 
         <div className="icons">
-
+          <RiCloseCircleLine />
+          <TiEdit />
         </div>
 
     </div>
-  )
+  ));
 }
 
 export default Todo
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -11,15 +12,26 @@ function TodoList() {
 
     const newTodos = [todo, ...todos]
 
-    setTodos(newTodos)
-    // console.log(...todos);
-    }
+    setTodos(newTodos);
+    console.log(...todos);
+    };
+
+    const completeTodo = id => {
+      let updateTodos = todos.map(todo => {
+        if(todo.id === id){
+          todo.isComplete = !todo.isComplete
+        }
+        return todo
+      });
+      setTodos(updateTodos);
+    };
 
   return (
     <div>
         <h1>What's the plan today?</h1>
-        {/* Import the form */}
+        
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} />
     </div>
   )
 }
