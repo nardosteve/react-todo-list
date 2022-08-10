eb4b9cca7bb74a4d5fa2deff7e88d097c8c6b192
nardosteve <jackcale50@gmail.com>
2022-08-10 13:54:43 +0300

    Fixing TodoList error

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,6 +5,7 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const [todos, setTodos] = useState([])
 
+    //Add todo
     const addTodo = todo => {
         if(!todo.text || /^\s*$/.test(todo.text)){
             return;
@@ -13,9 +14,25 @@ function TodoList() {
     const newTodos = [todo, ...todos]
 
     setTodos(newTodos);
-    console.log(...todos);
+    //console.log(...todos);
     };
 
+        //Edit todo
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    };
+
+    //Remove from list
+    const removeTodo = id => {
+      const removeArr = [...todos].filter(todo => todo.id !== id);
+
+      setTodos(removeArr);
+    };
+
+    //Hover if complete (toggle)
     const completeTodo = id => {
       let updateTodos = todos.map(todo => {
         if(todo.id === id){
@@ -31,7 +48,7 @@ function TodoList() {
         <h1>What's the plan today?</h1>
         
         <TodoForm onSubmit={addTodo} />
-        <Todo todos={todos} completeTodo={completeTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   )
 }
